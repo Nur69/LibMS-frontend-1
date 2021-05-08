f57a0bc3c4d0ab2788577ed953a5855ed279a0b6
MohamedBechir <jamoussi.bechir@yahoo.com>
2021-05-08 11:29:08 +0200

    refactor: refactor the sidebar component

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -1,54 +1,54 @@
 import React, { memo } from 'react';
-import { Col, Nav, Navbar, Row } from 'react-bootstrap';
-import { IconType } from 'react-icons/lib';
+import { Nav, Navbar, Row } from 'react-bootstrap';
+import LIBMSLOGO from '../../components/Sidebar/assets/LibMSLOGO.png';
+import * as IconName from 'react-icons/fa';
+import styled from 'styled-components/macro';
 
-interface Item {
-  name: string;
-  link: string;
-  icon: IconType;
-}
+const navItemsTop = [
+  { name: 'Dashboard', link: '/', icon: IconName.FaChartBar },
+  { name: 'Library', link: '/', icon: IconName.FaBook },
+  { name: 'Reservations', link: '/', icon: IconName.FaRegListAlt },
+  { name: 'Templates', link: '/', icon: IconName.FaColumns },
+  { name: 'Categories', link: '/', icon: IconName.FaTag },
+];
+const navItemsBottom = [
+  { name: 'Setting', link: '/', icon: IconName.FaCog },
+  { name: 'Support', link: '/', icon: IconName.FaQuestionCircle },
+];
+const imgSource = LIBMSLOGO;
 
-interface Props {
-  navItemsTop: Item[];
-  imgSource: string;
-  navItemsBottom: Item[];
-}
+export const Sidebar = memo(() => {
+  return (
+    <>
+      <div className="min-vh-100">
+        <Row>
+          <Navbar variant="light">
+            <Nav className="flex-column mb-auto">
+              <Row className="mb-5">
+                <img src={imgSource} alt="" width={200} height={101} />
+              </Row>
+              <Row className="flex-column mt-5 mb-5">
+                {navItemsTop.map(navigate => (
+                  <CustomNavLink href={navigate.link}>
+                    <navigate.icon className="mr-2" />
+                    {navigate.name}
+                  </CustomNavLink>
+                ))}
+              </Row>
+              <Row className="flex-column mt-5">
+                {navItemsBottom.map(navigate => (
+                  <CustomNavLink href={navigate.link}>
+                    <navigate.icon className="mr-2" />
+                    {navigate.name}
+                  </CustomNavLink>
+                ))}
+              </Row>
+            </Nav>
+          </Navbar>
+        </Row>
+      </div>
+    </>
+  );
+});
 
-export const Sidebar = memo(
-  ({ navItemsTop, navItemsBottom, imgSource }: Props) => {
-    return (
-      <>
-        <div className="min-vh-100">
-          <Row>
-            <Col xs={2} className="d-flex min-vh-100 justify-content-center">
-              <Navbar variant="light">
-                <Nav className="flex-column mb-auto">
-                  <Row className="mb-5">
-                    <img src={imgSource} alt="" width={200} height={101} />
-                  </Row>
-                  <Row className="flex-column mt-5 mb-5">
-                    {navItemsTop.map(navigate => (
-                      <Nav.Link href={navigate.link}>
-                        <navigate.icon className="mr-2" />
-                        {navigate.name}
-                      </Nav.Link>
-                    ))}
-                  </Row>
-                  <Row className="flex-column mt-5">
-                    {navItemsBottom.map(navigate => (
-                      <Nav.Link href={navigate.link}>
-                        <navigate.icon className="mr-2" />
-                        {navigate.name}
-                      </Nav.Link>
-                    ))}
-                  </Row>
-                </Nav>
-              </Navbar>
-            </Col>
-            <Col xs={10} className="bg-light"></Col>
-          </Row>
-        </div>
-      </>
-    );
-  },
-);
+const CustomNavLink = styled(Nav.Link)``;
--- src/app/pages/DashboardPage/index.tsx
+++ src/app/pages/DashboardPage/index.tsx
@@ -1,27 +1,13 @@
 import { Sidebar } from 'app/components/Sidebar';
-import React from 'react';
-import LIBMSLOGO from '../../components/Sidebar/assets/LibMSLOGO.png';
-import * as IconName from 'react-icons/fa';
+import { Col } from 'react-bootstrap';
 
 export function DashboardPage() {
-  const navItemsTop = [
-    { name: 'Dashboard', link: '/', icon: IconName.FaChartBar },
-    { name: 'Library', link: '/', icon: IconName.FaBook },
-    { name: 'Reservations', link: '/', icon: IconName.FaRegListAlt },
-    { name: 'Templates', link: '/', icon: IconName.FaColumns },
-    { name: 'Categories', link: '/', icon: IconName.FaTag },
-  ];
-  const navItemsBottom = [
-    { name: 'Setting', link: '/', icon: IconName.FaCog },
-    { name: 'Support', link: '/', icon: IconName.FaQuestionCircle },
-  ];
   return (
     <>
-      <Sidebar
-        imgSource={LIBMSLOGO}
-        navItemsTop={navItemsTop}
-        navItemsBottom={navItemsBottom}
-      ></Sidebar>
+      <Col xs={2} className="d-flex min-vh-100 justify-content-center">
+        <Sidebar></Sidebar>
+      </Col>
+      <Col xs={10} className="bg-light"></Col>
     </>
   );
 }
