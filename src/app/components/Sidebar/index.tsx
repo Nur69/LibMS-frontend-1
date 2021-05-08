04b95e049c224afea2b7f5cde88b675e7ea85f2b
MohamedBechir <jamoussi.bechir@yahoo.com>
2021-05-08 02:48:49 +0200

    feat: implement the sidebar as a component

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -1,2 +1,54 @@
-interface props {}
-export const sidebar = {};
+import React, { memo } from 'react';
+import { Col, Nav, Navbar, Row } from 'react-bootstrap';
+import { IconType } from 'react-icons/lib';
+
+interface Item {
+  name: string;
+  link: string;
+  icon: IconType;
+}
+
+interface Props {
+  navItemsTop: Item[];
+  imgSource: string;
+  navItemsBottom: Item[];
+}
+
+export const Sidebar = memo(
+  ({ navItemsTop, navItemsBottom, imgSource }: Props) => {
+    return (
+      <>
+        <div className="min-vh-100">
+          <Row>
+            <Col xs={2} className="d-flex min-vh-100 justify-content-center">
+              <Navbar variant="light">
+                <Nav className="flex-column mb-auto">
+                  <Row className="mb-5">
+                    <img src={imgSource} alt="" width={200} height={101} />
+                  </Row>
+                  <Row className="flex-column mt-5 mb-5">
+                    {navItemsTop.map(navigate => (
+                      <Nav.Link href={navigate.link}>
+                        <navigate.icon className="mr-2" />
+                        {navigate.name}
+                      </Nav.Link>
+                    ))}
+                  </Row>
+                  <Row className="flex-column mt-5">
+                    {navItemsBottom.map(navigate => (
+                      <Nav.Link href={navigate.link}>
+                        <navigate.icon className="mr-2" />
+                        {navigate.name}
+                      </Nav.Link>
+                    ))}
+                  </Row>
+                </Nav>
+              </Navbar>
+            </Col>
+            <Col xs={10} className="bg-light"></Col>
+          </Row>
+        </div>
+      </>
+    );
+  },
+);
--- src/app/pages/DashboardPage/index.tsx
+++ src/app/pages/DashboardPage/index.tsx
@@ -1,58 +1,27 @@
+import { Sidebar } from 'app/components/Sidebar';
 import React from 'react';
-import { Col, Nav, Navbar, Row } from 'react-bootstrap';
+import LIBMSLOGO from '../../components/Sidebar/assets/LibMSLOGO.png';
 import * as IconName from 'react-icons/fa';
 
-import LIBMSLOGO from './assets/LibMSLOGO.png';
-
 export function DashboardPage() {
+  const navItemsTop = [
+    { name: 'Dashboard', link: '/', icon: IconName.FaChartBar },
+    { name: 'Library', link: '/', icon: IconName.FaBook },
+    { name: 'Reservations', link: '/', icon: IconName.FaRegListAlt },
+    { name: 'Templates', link: '/', icon: IconName.FaColumns },
+    { name: 'Categories', link: '/', icon: IconName.FaTag },
+  ];
+  const navItemsBottom = [
+    { name: 'Setting', link: '/', icon: IconName.FaCog },
+    { name: 'Support', link: '/', icon: IconName.FaQuestionCircle },
+  ];
   return (
     <>
-      <div className="min-vh-100">
-        <Row>
-          <Col xs={2} className="d-flex min-vh-100 justify-content-center">
-            <Navbar variant="light">
-              <Nav className="flex-column mb-auto">
-                <Row className="mb-5">
-                  <img src={LIBMSLOGO} alt="" width={200} height={101} />
-                </Row>
-                <Row className="flex-column mt-5 mb-5">
-                  <Nav.Link href="/home">
-                    <IconName.FaChartBar className="mr-2" />
-                    Dashboard
-                  </Nav.Link>
-                  <Nav.Link eventKey="link-1">
-                    <IconName.FaBook className="mr-2" />
-                    Library
-                  </Nav.Link>
-                  <Nav.Link eventKey="link-2">
-                    <IconName.FaRegListAlt className="mr-2" />
-                    Reservations
-                  </Nav.Link>
-                  <Nav.Link eventKey="link-3">
-                    <IconName.FaColumns className="mr-2" />
-                    Templates
-                  </Nav.Link>
-                  <Nav.Link eventKey="link-3">
-                    <IconName.FaTag className="mr-2" />
-                    Categories
-                  </Nav.Link>
-                </Row>
-                <Row className="flex-column mt-5">
-                  <Nav.Link eventKey="link-3">
-                    <IconName.FaCog className="mr-2" />
-                    Settings
-                  </Nav.Link>
-                  <Nav.Link eventKey="link-3">
-                    <IconName.FaQuestionCircle className="mr-2" />
-                    Support
-                  </Nav.Link>
-                </Row>
-              </Nav>
-            </Navbar>
-          </Col>
-          <Col xs={10} className="bg-light"></Col>
-        </Row>
-      </div>
+      <Sidebar
+        imgSource={LIBMSLOGO}
+        navItemsTop={navItemsTop}
+        navItemsBottom={navItemsBottom}
+      ></Sidebar>
     </>
   );
 }
