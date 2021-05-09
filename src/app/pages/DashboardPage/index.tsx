61b2a670528db17b3ce3dcf6a9a41b45b7629435
MohamedBechir <jamoussi.bechir@yahoo.com>
2021-05-10 01:18:13 +0200

    refactor: refactor the dashboard page design

--- src/app/pages/DashboardPage/index.tsx
+++ src/app/pages/DashboardPage/index.tsx
@@ -1,13 +1,15 @@
 import { Sidebar } from 'app/components/Sidebar';
-import { Col } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 
 export function DashboardPage() {
   return (
     <>
-      <Col xs={2} className="d-flex min-vh-100 justify-content-center">
-        <Sidebar></Sidebar>
-      </Col>
-      <Col xs={10} className="bg-light"></Col>
+      <Row>
+        <Col xs={2} className="d-flex min-vh-100 justify-content-center">
+          <Sidebar></Sidebar>
+        </Col>
+        <Col xs={10} className="bg-light"></Col>
+      </Row>
     </>
   );
 }
