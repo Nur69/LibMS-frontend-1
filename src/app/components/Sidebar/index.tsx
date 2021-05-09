d519bc801874ed8a1c03944be8223051bc033055
MohamedBechir <jamoussi.bechir@yahoo.com>
2021-05-10 01:19:20 +0200

    refactor: refactor the sidebar design

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -1,19 +1,19 @@
 import React, { memo } from 'react';
 import { Nav, Navbar, Row } from 'react-bootstrap';
-import LIBMSLOGO from '../../components/Sidebar/assets/LibMSLOGO.png';
+import LIBMSLOGO from './assets/LibMSLOGO.png';
 import * as IconName from 'react-icons/fa';
-import styled from 'styled-components/macro';
+import { Link } from 'react-router-dom';
 
 const navItemsTop = [
-  { name: 'Dashboard', link: '/', icon: IconName.FaChartBar },
-  { name: 'Library', link: '/', icon: IconName.FaBook },
-  { name: 'Reservations', link: '/', icon: IconName.FaRegListAlt },
-  { name: 'Templates', link: '/', icon: IconName.FaColumns },
-  { name: 'Categories', link: '/', icon: IconName.FaTag },
+  { name: 'Dashboard', link: '/dashboard', icon: IconName.FaChartBar },
+  { name: 'Library', link: '/books', icon: IconName.FaBook },
+  { name: 'Reservations', link: '/dashboard', icon: IconName.FaRegListAlt },
+  { name: 'Templates', link: '/dashboard', icon: IconName.FaColumns },
+  { name: 'Categories', link: '/dashboard', icon: IconName.FaTag },
 ];
 const navItemsBottom = [
-  { name: 'Setting', link: '/', icon: IconName.FaCog },
-  { name: 'Support', link: '/', icon: IconName.FaQuestionCircle },
+  { name: 'Setting', link: '/dashboard', icon: IconName.FaCog },
+  { name: 'Support', link: '/dashboard', icon: IconName.FaQuestionCircle },
 ];
 const imgSource = LIBMSLOGO;
 
@@ -21,34 +21,30 @@ export const Sidebar = memo(() => {
   return (
     <>
       <div className="min-vh-100">
-        <Row>
-          <Navbar variant="light">
-            <Nav className="flex-column mb-auto">
-              <Row className="mb-5">
-                <img src={imgSource} alt="" width={200} height={101} />
-              </Row>
-              <Row className="flex-column mt-5 mb-5">
-                {navItemsTop.map((navigate, i) => (
-                  <CustomNavLink href={navigate.link} key={i}>
-                    <navigate.icon className="mr-2" />
-                    {navigate.name}
-                  </CustomNavLink>
-                ))}
-              </Row>
-              <Row className="flex-column mt-5">
-                {navItemsBottom.map((navigate, i) => (
-                  <CustomNavLink href={navigate.link} key={i}>
-                    <navigate.icon className="mr-2" />
-                    {navigate.name}
-                  </CustomNavLink>
-                ))}
-              </Row>
-            </Nav>
-          </Navbar>
-        </Row>
+        <Navbar variant="light">
+          <Nav className="flex-column">
+            <Row className="mb-5">
+              <img src={imgSource} alt="" width={200} height={101} />
+            </Row>
+            <Row className="flex-column mt-5 mb-5">
+              {navItemsTop.map((navigate, i) => (
+                <Nav.Link as={Link} to={navigate.link} key={i}>
+                  <navigate.icon className="mr-2" />
+                  {navigate.name}
+                </Nav.Link>
+              ))}
+            </Row>
+            <Row className="flex-column mt-5">
+              {navItemsBottom.map((navigate, i) => (
+                <Nav.Link as={Link} to={navigate.link} key={i}>
+                  <navigate.icon className="mr-2" />
+                  {navigate.name}
+                </Nav.Link>
+              ))}
+            </Row>
+          </Nav>
+        </Navbar>
       </div>
     </>
   );
 });
-
-const CustomNavLink = styled(Nav.Link)``;
