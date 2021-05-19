9d29b72dcbf6a775c2b29e687059632129b45cf9
MohamedBechir <jamoussi.bechir@yahoo.com>
2021-05-20 00:18:40 +0200

    refactor: update component to confirm or deny reservation requests

--- src/app/pages/LibrarianPages/ReservationsPage/ReservationsList/index.tsx
+++ src/app/pages/LibrarianPages/ReservationsPage/ReservationsList/index.tsx
@@ -1,5 +1,5 @@
 import { memo, useEffect } from 'react';
-import { Table } from 'react-bootstrap';
+import { Table, Col, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { useFetchReservationsSlice } from './slice';
 import { selectReservations } from './slice/selectors';
@@ -7,11 +7,11 @@ import { selectReservations } from './slice/selectors';
 const features = [
   'Book',
   'ISBN',
-  'Number Of Copies',
-  'Email',
+  'Copies',
   'University ID',
   'Date of Reservation',
   'Status',
+  'Actions',
 ];
 export const ReservationsList = memo(() => {
   const { actions } = useFetchReservationsSlice();
@@ -27,9 +27,10 @@ export const ReservationsList = memo(() => {
   });
 
   return (
-    <Table className="w-75" striped bordered hover>
+    <Table className="w-100 mr-5 ml-5" striped bordered hover>
       <thead>
         <tr
+          className="text-center"
           style={{
             backgroundColor: '#707070',
             color: '#E5E5E5',
@@ -43,16 +44,47 @@ export const ReservationsList = memo(() => {
       </thead>
       <tbody>
         {reservationsSelected.reservations.map(reservation => (
-          <tr>
-            <td>{reservation.book.title}</td>
-            <td>{reservation.book.isbn}</td>
-            <td>{reservation.book.copiesNbr}</td>
-            <td>{reservation.user.email}</td>
-            <td>{reservation.user.universityID}</td>
-            <td>{reservation.reservedAt.substring(0, 10)}</td>
-            <td>
-              {reservation.reservationStatus.charAt(0).toUpperCase() +
-                reservation.reservationStatus.slice(1)}
+          <tr className="text-center">
+            <td className="col-2">{reservation.book.title}</td>
+            <td className="col-1">{reservation.book.isbn}</td>
+            <td className="col-1">{reservation.book.copiesNbr}</td>
+            <td className="col-2">{reservation.user.universityID}</td>
+            <td className="col-2">{reservation.reservedAt.substring(0, 10)}</td>
+            <td className="col-1">
+              {(() => {
+                if (reservation.reservationStatus === 'pending') {
+                  return (
+                    <div className="badge badge-pill badge-warning">
+                      {reservation.reservationStatus.charAt(0).toUpperCase() +
+                        reservation.reservationStatus.slice(1)}
+                    </div>
+                  );
+                } else if (reservation.reservationStatus === 'active') {
+                  return (
+                    <span className="badge badge-pill badge-success">
+                      {reservation.reservationStatus.charAt(0).toUpperCase() +
+                        reservation.reservationStatus.slice(1)}
+                    </span>
+                  );
+                } else if (reservation.reservationStatus === 'rejected') {
+                  return (
+                    <span className="badge badge-pill badge-danger">
+                      {reservation.reservationStatus.charAt(0).toUpperCase() +
+                        reservation.reservationStatus.slice(1)}
+                    </span>
+                  );
+                }
+              })()}
+            </td>
+            <td className="col-3">
+              <div className="d-flex flex-row">
+                <Col xs={6}>
+                  <Button className="w-100 btn-success btn-sm">Accept</Button>
+                </Col>
+                <Col xs={6}>
+                  <Button className="btn-danger w-100 btn-sm">Deny</Button>
+                </Col>
+              </div>
             </td>
           </tr>
         ))}
